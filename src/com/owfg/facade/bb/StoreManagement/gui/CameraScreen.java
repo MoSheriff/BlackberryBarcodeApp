b973ca66713c70460091aedfacaedd8bbe6415ac
Mohamed Sheriffdeen <m.scelus.s@gmail.com>
2011-05-06 11:48:30 -0700

    Updated packages. Probably borked it.

---
+++ src/com/owfg/facade/bb/StoreManagement/gui/CameraScreen.java
@@ -0,0 +1,28 @@
+package com.owfg.facade.bb.StoreManagement.gui;
+
+import com.owfg.facade.bb.StoreManagement.app.ScreenshotThread;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+public final class CameraScreen extends MainScreen {
+	CameraThread cThread;
+
+	public CameraScreen() {
+		cThread = new CameraThread();
+	}
+	
+	public void startThread() {
+		cThread.start();
+	}
+	
+	class CameraThread extends Thread {	
+		Runnable imgTkr;
+		CameraThread() {
+		}
+		
+		public void run() {
+			imgTkr = new ScreenshotThread();
+			imgTkr.run();
+		}
+	}
+}
