f0312b5742881f82b2adb92441e396fa69a3a5cc
Warren Voelkl <warren_voelkl@hotmail.com>
2011-05-06 11:29:27 -0700

    new stub versions with a better package structure

---
+++ src/com/owfg/facade/bb/StoreManagement/Logger/Logger.java
@@ -0,0 +1,34 @@
+package com.owfg.facade.bb.StoreManagement.Logger;
+
+import mypackage.MyApp;
+import net.rim.device.api.system.EventLogger;
+
+public class Logger {
+	private static void logEvent(String msg, int level) {
+		EventLogger.logEvent(MyApp.GUID, msg.getBytes(), level);
+	}
+
+	public static void logDebugEvent(String msg) {
+		logEvent(msg, EventLogger.DEBUG_INFO);
+	}
+
+	public static void logInformationEvent(String msg) {
+		logEvent(msg, EventLogger.INFORMATION);
+	}
+
+	public static void logWarningEvent(String msg) {
+		logEvent(msg, EventLogger.WARNING);
+	}
+
+	public static void logErrorEvent(String msg) {
+		logEvent(msg, EventLogger.ERROR);
+	}
+
+	public static void logSevereErrorEvent(String msg) {
+		logEvent(msg, EventLogger.SEVERE_ERROR);
+	}
+
+	public static void logAlwaysEvent(String msg) {
+		logEvent(msg, EventLogger.ALWAYS_LOG);
+	}
+}
---
+++ src/com/owfg/facade/bb/StoreManagement/weatherExample/WeatherScreen.java
@@ -0,0 +1,72 @@
+package com.owfg.facade.bb.StoreManagement.weatherExample;
+
+import com.owfg.facade.bb.StoreManagement.stub.WeatherForecastSoap_Stub;
+import com.owfg.facade.bb.StoreManagement.stub.WeatherForecasts;
+
+import mypackage.MyApp;
+import net.rim.device.api.ui.MenuItem;
+import net.rim.device.api.ui.component.Dialog;
+import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
+import net.rim.device.api.ui.component.Menu;
+import net.rim.device.api.ui.component.RichTextField;
+import net.rim.device.api.ui.container.MainScreen;
+
+/**
+ * A class extending the MainScreen class, which provides default standard
+ * behavior for BlackBerry GUI applications.
+ */
+public final class WeatherScreen extends MainScreen
+{
+	private WeatherForecastSoap_Stub stub = null;
+	private WeatherForecasts response = null;
+	private String location = "Boston";
+	private RichTextField field;
+	private MyApp app;
+	private EditField editSearch;
+	private RichTextField searchString;
+	
+    public WeatherScreen(MyApp application)
+    {   
+    	super(DEFAULT_MENU | DEFAULT_CLOSE);
+    	app = application;
+    	
+    	setTitle(new LabelField("Contacting a weather service", LabelField.ELLIPSIS | LabelField.USE_ALL_WIDTH));
+    	field = new RichTextField("");
+    	searchString = new RichTextField("");
+    	add(editSearch = new EditField(EditField.FILTER_DEFAULT));
+    	add(searchString);
+    	add(field);
+    }
+    
+	public void makeMenu(Menu menu, int instance) {
+		menu.add(getResponse);
+	}
+	
+    public MenuItem getResponse = new MenuItem("Get response",100, 1) {
+		public void run() {
+			location = editSearch.getText();
+			if (location.length() == 0) {
+				return;
+			}
+			searchString.setText("Querying for location: " + location);
+			//field.setText(location);
+			editSearch.setText("");
+			try {
+				if (stub == null) {
+					stub = new WeatherForecastSoap_Stub();
+				}
+				response = stub.getWeatherByPlaceName(location);
+				searchString.setText("Found: " + response.getPlaceName() + ", " + response.getStateCode());
+				field.setText("Latitude: " + Float.toString(response.getLatitude()) + " Longitude: " + Float.toString(response.getLongitude())) ;
+				//editList.insert(2);
+				//myCallback.insert(response.getStatus(), 2);
+				//editList.setSelectedIndex(0);
+			} catch (Exception ex) {
+				Dialog.alert("Unable to connect to service. Service returned:\n"
+						+ ex.toString());
+			}
+		}
+	};                                   
+		
+}
