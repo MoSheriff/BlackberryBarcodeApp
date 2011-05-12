ac2a7797fd3dff6a776f26ef16be8e2fa0cb35ab
Mohamed Sheriffdeen <m.scelus.s@gmail.com>
2011-05-11 20:39:38 -0700

    basic login screen changes

--- src/main/java/com/owfg/facade/bb/StoreManagement/gui/LoginScreen.java
+++ src/main/java/com/owfg/facade/bb/StoreManagement/gui/LoginScreen.java
@@ -8,35 +8,40 @@ import net.rim.device.api.ui.component.ButtonField;
 import net.rim.device.api.ui.component.EditField;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.PasswordEditField;
+import net.rim.device.api.ui.container.HorizontalFieldManager;
 import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.container.VerticalFieldManager;
 
 public class LoginScreen extends MainScreen {
 	final int enterButton = 4;
+	Field okButton;
 
 	public LoginScreen() {
 		Bitmap backgroundBitmap = Bitmap.getBitmapResource("logo_ofg_72-1.png");
 		LabelField title = new LabelField("Login", LabelField.ELLIPSIS
 				| LabelField.USE_ALL_WIDTH);
 		setTitle(title);
+		HorizontalFieldManager hfm = new HorizontalFieldManager(FIELD_BOTTOM);
 		VerticalFieldManager vfm = new VerticalFieldManager();
 		FieldChangeListener buttonListener = new ButtonListener();
 
 		EditField username = new EditField();
 		PasswordEditField passwd = new PasswordEditField();
-		Field okButton = new ButtonField("Enter", ButtonField.CONSUME_CLICK);
+		okButton = new ButtonField("Enter", ButtonField.CONSUME_CLICK);
 		okButton.setChangeListener(buttonListener);
 		vfm.add(new LabelField("Username:"));
 		vfm.add(username);
 		vfm.add(new LabelField("Password:"));
 		vfm.add(passwd);
 		vfm.add(okButton);
-		add(vfm);
+		hfm.add(vfm);
+		//hfm.setPositionChild(vfm, 100, 100);
+		add(hfm);
 	}
 
 	final class ButtonListener implements FieldChangeListener {
 		public void fieldChanged(Field field, int context) {
-			if (field.getIndex() == enterButton) {
+			if (field.equals(okButton)) {
 				MyApp.cScreen.startThread();
 				synchronized (MyApp.app.getEventLock()) {
 					MyApp.app.popScreen(getScreen());
